d5b595a10a03e26c33c539f576a9269ed1a71083
Nikola Lakobrija <95445072+LakiTheGreat@users.noreply.github.com>
2023-07-18 17:32:05 +0200

    Delete build directory

--- build/middleware/checkIdFormat.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function checkIdFormat(req, res, next) {
-    return req; //TODO
-    next();
-}
-exports.default = checkIdFormat;
