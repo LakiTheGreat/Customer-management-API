9c46f389725a41a92a107223103027fa96c500f1
Nikola Lakobrija <lakobrija93@gmail.com>
2023-07-25 13:50:01 +0200

    replaced console.log with simple winston logger

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 import app from "./app";
 import { mongooseObj } from "./config";
+import { logger } from "./config/logger";
 
 mongoose.connect(mongooseObj.url, mongooseObj.options).then(() => {
-  console.log("Connected to MongoDB");
+  logger.info("Connected to MongoDB");
   app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${process.env.PORT}...`);
+    logger.info(`App running on port ${process.env.PORT}...`);
   });
 });
