f8eacee9eea0e279899a5f03156544573ed663c2
Nikola Lakobrija <lakobrija93@gmail.com>
2023-07-25 14:17:34 +0200

    use morgan only in dev

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,16 @@ import { router } from "./routes";
 
 const app = express();
 
-// 1) Middlewares
-app.use(morgan("dev")); //logs request data in terminal
-app.use(express.json()); //allows the use of .body in requst (req.body)
-
-// 2) Routes
-app.use("/v1", router);
-
-// 3) Allows the use of .env file and process.env variable
+// 1) Allows the use of .env file and process.env variable
 dotenv.config({
   path: ".env",
 });
+
+// 2) Middlewares
+if (process.env.NODE_ENV === "development") app.use(morgan("dev")); //logs request data in terminal
+app.use(express.json()); //allows the use of .body in requst (req.body)
+
+// 3) Routes
+app.use("/v1", router);
 
 export default app;
